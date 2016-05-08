9b372200c67d67f09c8999f13c5e9bd2a79d08f2
nyg0o0 <yaya56790@gmail.com>
2016-05-09 00:17:38 +0800

    註解

--- TestNumbers.java
+++ TestNumbers.java
@@ -1,9 +1,9 @@
 // import java API
 import java.util.Scanner;
-import java.math.BigDecimal;
+
 public class TestNumbers {
 	public static void main(String[] args) {
-		// new object for scanner
+		// new an object for scanner
 		Scanner input = new Scanner(System.in);
 		
 		// Prompt user to enter numbers
@@ -11,37 +11,47 @@ public class TestNumbers {
 		double number1 = input.nextDouble();
 		System.out.print("Please enter another number: ");
 		double number2 = input.nextDouble();
+		
+		// choices of operation for users
 		System.out.println("---------------------------------");
 		System.out.println("Press 1 for doing addition ");
 		System.out.println("Press 2 for doing subtraction ");
 		System.out.println("Press 3 for doing multiplication ");
 		System.out.println("Press 4 for doing division ");
 		System.out.println("---------------------------------");
+		
+		// scanner the options
 		System.out.print("Your choice: ");
 		int operateType = input.nextInt();
 		System.out.println("---------------------------------");
+		
+		// create an object for BigDecimalNum class
 		BigDecimalNum operation = new BigDecimalNum(number1,number2);
+		
+		/* switchs for operation*/
 		switch(operateType){
-			case 1:
+			case 1: // add
 				System.out.println(number1 + " + " + number2 + " = " + operation.add());
 				break;
-			case 2:
+			case 2:	// subtract
 				System.out.println(number1 + " - " + number2 + " = " + operation.subtract());
 				break;
-			case 3:
+			case 3:	// multiply
 				System.out.println(number1 + " * " + number2 + " = " + operation.multiply());
 				break;
-			case 4:
-				if( number2 == 0 ){
+			case 4:	// divide
+				if( number2 == 0 ){	// if dividend is equal to zero, can not doing operation
 					System.out.println("You can not divide by zero!!");
 					System.exit(1);
 				}
+				
+				// to make it more precise, scan the scale from user input
 				System.out.print("Please enter the scale after decimal point: ");
 				int scaleAfterPoint = input.nextInt();
-				if( scaleAfterPoint < 0 ){
+				if( scaleAfterPoint < 0 ){	// illegal scale
 					System.out.println("Your input is illegal!");
 				}
-				else{
+				else{	// if scale is legal, show the answer
 					System.out.println(number1 + " / " + number2 + " = " + operation.divide(scaleAfterPoint));
 				}
 				break;
